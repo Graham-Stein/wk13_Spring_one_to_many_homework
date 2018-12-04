c100ab3b734a1aa7a8b6b5fac2c5741596aef1ec
Graham-Stein <graham@guavoaviation.co.uk>
2018-12-04 16:10:17 +0000

    Add start point

---
+++ src/main/java/com/example/hwOneToMany/HwOneToManyApplication.java
@@ -0,0 +1,12 @@
+package com.example.hwOneToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwOneToManyApplication.class, args);
+	}
+}
